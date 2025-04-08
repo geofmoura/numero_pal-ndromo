cbac73ec33c02be36091db6550eadb7b979c5886
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-04-07 22:03:14 -0300

    Desafio de Palíndromo em C

--- palindromo.c
+++ palindromo.c
@@ -8,16 +8,19 @@ int main() {
     printf("Digite um número de 4 dígitos: ");
     scanf("%d", &numero);
 
+    // verifica se o numero tem exatamente 4 digitos
     if (numero < 1000 || numero > 9999) {
         printf("O número deve ter exatamente 4 dígitos.\n");
         return 0;
     }
 
+    // extrai os digitos
     a = numero / 1000;
     b = (numero / 100) % 10;
     c = (numero / 10) % 10;
     d = numero % 10;
 
+    // verifica se é palindromo
     if (a == d && b == c) {
         printf("É um número palíndromo.\n");
     } else {
